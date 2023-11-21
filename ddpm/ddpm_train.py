942ef013fe62ac5b658242668b83379f64dea2ff
jakwisn <jakwisn@gmail.com>
2023-11-21 14:11:12 +0100

    working demo

--- ddpm/ddpm_train.py
+++ ddpm/ddpm_train.py
@@ -86,24 +86,24 @@ def train(dataloader, valdataloader, device='cpu', T=500, img_size=16, input_cha
         with torch.enable_grad():
             for i, (images, masks) in enumerate(pbar):
                 images = images.to(device)
-                masks = masks.to(device)
+                masks = masks.to(device).unsqueeze(1)
 
                 t = diffusion.sample_timesteps(images.shape[0]).to(device) # line 3 from the Training algorithm
 
                 # q sample does is done just like before
                 x_t, noise = diffusion.q_sample(images, t)
 
-                known_regions = x_t * ~masks.unsqueeze(1)
+                known_regions = x_t * ~masks
                 x_p = diffusion.p_sample(model, x_t, t)
-                unknown_regions = x_p * masks.unsqueeze(1)
+
+                unknown_regions = x_p * masks
 
                 x_t = known_regions + unknown_regions
 
                 predicted_noise = model(x_t, t) # predict noise of x_t using the UNet
+                unknown_regions = predicted_noise * masks
 
-                unknown_regions = predicted_noise * masks.unsqueeze(1)
-
-                loss = mse(noise*masks.unsqueeze(1), unknown_regions) # loss between masked noise and masked predicted noise
+                loss = mse(noise*masks, unknown_regions) # loss between masked noise and masked predicted noise
 
                 optimizer.zero_grad()
                 loss.backward()
@@ -111,13 +111,125 @@ def train(dataloader, valdataloader, device='cpu', T=500, img_size=16, input_cha
 
                 pbar.set_postfix(MSE=loss.item())
                 logger.add_scalar("MSE", loss.item(), global_step=epoch * l + i)
-
+             
         with torch.no_grad():
             pbar = tqdm(valdataloader)
             for i, (images, masks) in enumerate(pbar):
                 images = images.to(device)
                 masks = masks.to(device)
             sampled_images = diffusion.p_sample_loop(images, masks, model,  batch_size=images.shape[0])
+            save_images(images=sampled_images, path=os.path.join("results", experiment_name, f"{epoch}.jpg"),
+                        show=show, title=f'Epoch {epoch}')
+
+        torch.save(model.state_dict(), os.path.join("models", experiment_name, f"weights-{epoch}.pt"))
+
+
+def train_profiler(dataloader, valdataloader, device='cpu', T=500, img_size=16, input_channels=3, channels=32, time_dim=256,
+          batch_size=100, lr=1e-3, num_epochs=30, experiment_name="ddpm", show=False):
+    """Implements algrorithm 1 (Training) from the ddpm paper at page 4"""
+    create_result_folders(experiment_name)
+
+    if not dataloader:
+        dataloader = prepare_dataloader(batch_size)
+
+    model = UNet(img_size=img_size, c_in=input_channels, c_out=input_channels,
+                 time_dim=time_dim, channels=channels, device=device).to(device)
+    diffusion = Diffusion(img_size=img_size, T=T, beta_start=1e-4, beta_end=0.02, device=device)
+
+    ckpt = torch.load("checkpoints/unconditional_ckpt.pt")
+    model.load_state_dict(ckpt)
+    optimizer = optim.AdamW(model.parameters(), lr=lr)
+    mse = torch.nn.MSELoss()  # use MSE loss
+
+    logger = SummaryWriter(os.path.join("runs", experiment_name))
+    l = len(dataloader)
+
+    # PROFILER
+    wait = 1
+    warmup = 1
+    active = 3
+    repeat = 5
+    prof = torch.profiler.profile(
+        schedule=torch.profiler.schedule(wait=wait, warmup=warmup, active=active, repeat=repeat),
+        on_trace_ready=torch.profiler.tensorboard_trace_handler('./prof/train'),
+        record_shapes=True,
+        with_stack=False)
+    prof.start()
+    prof_step = 0
+
+    for epoch in range(1, num_epochs + 1):
+        logging.info(f"Starting epoch {epoch}:")
+        pbar = tqdm(dataloader)
+
+        with torch.enable_grad():
+            for i, (images, masks) in enumerate(pbar):
+
+                # Torch profiler
+                torch.cuda.synchronize()
+
+                images = images.to(device)
+                masks = masks.to(device)
+
+                # Torch profiler
+                torch.cuda.synchronize()
+
+                t = diffusion.sample_timesteps(images.shape[0]).to(device)  # line 3 from the Training algorithm
+
+                # q sample does is done just like before
+                x_t, noise = diffusion.q_sample(images, t)
+                # Torch profiler
+                torch.cuda.synchronize()
+
+                known_regions = x_t * ~masks.unsqueeze(1)
+                x_p = diffusion.p_sample(model, x_t, t)
+
+                # Torch profiler
+                torch.cuda.synchronize()
+
+                unknown_regions = x_p * masks.unsqueeze(1)
+
+                x_t = known_regions + unknown_regions
+
+                # Torch profiler
+                torch.cuda.synchronize()
+
+                predicted_noise = model(x_t, t)  # predict noise of x_t using the UNet
+
+                # Torch profiler
+                torch.cuda.synchronize()
+
+                unknown_regions = predicted_noise * masks.unsqueeze(1)
+
+                loss = mse(noise * masks.unsqueeze(1),
+                           unknown_regions)  # loss between masked noise and masked predicted noise
+
+                # Torch profiler
+                torch.cuda.synchronize()
+
+                optimizer.zero_grad()
+                loss.backward()
+                # Torch profiler
+                torch.cuda.synchronize()
+
+                optimizer.step()
+
+                pbar.set_postfix(MSE=loss.item())
+                logger.add_scalar("MSE", loss.item(), global_step=epoch * l + i)
+                prof_step += 1
+                prof.step()
+
+                if prof_step >= (wait + warmup + active) * repeat:
+                    break
+        print('Stopped profiling')
+        prof.stop()
+        break
+
+        with torch.no_grad():
+            pbar = tqdm(valdataloader)
+            for i, (images, masks) in enumerate(pbar):
+                images = images.to(device)
+                masks = masks.to(device)
+            sampled_images = diffusion.p_sample_loop(images, masks, model, batch_size=images.shape[0])
             save_images(images=sampled_images, path=os.path.join("results", experiment_name, f"{epoch}.jpg"),
                         show=show, title=f'Epoch {epoch}')
 
